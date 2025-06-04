18b18f0cabd110041daa1174addba573974f7dbd
Polina <dfessica@gmail.com>
2025-06-04 14:03:47 +0300

    added new task

--- VoskresenskayaPolina/src/Module3/Easy/homework.java
+++ VoskresenskayaPolina/src/Module3/Easy/homework.java
@@ -1,9 +1,10 @@
-package VariableExample;
+package Module3.Easy;
 
 import java.util.Scanner;
 
 public class homework {
-    public static void main(String[] args) {Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите значение для а: ");
         int a = scanner.nextInt();
         System.out.println("Введите значение для b: ");
---
+++ VoskresenskayaPolina/src/Module3/Easy/homework_2.java
@@ -0,0 +1,18 @@
+package Module3.Easy;
+
+import java.util.Scanner;
+
+public class homework_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите вещественное число a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Введите вещественнное число b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Введите вещественнное число с: ");
+        double c = scanner.nextDouble();
+        double sum = a+b+c;
+        double mult = a*b*c;
+        System.out.println("Сумма a, b, c = "+ sum + " Произведение a, b, c = "+ mult);
+    }
+}
