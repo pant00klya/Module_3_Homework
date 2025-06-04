79b8c85b589a24d5a74ed64d50217bd79ae2c1c7
Polina <dfessica@gmail.com>
2025-06-04 13:47:07 +0300

    Initial commit

---
+++ src/VariableExample/homework.java
@@ -0,0 +1,16 @@
+package VariableExample;
+
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args) {Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение для а: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите значение для b: ");
+        int b = scanner.nextInt();
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println("Пользователь ввел значение для a: " + a + " и b: "+ b );
+    }
+}
