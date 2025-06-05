8b30b679c45986439f7d53d14598c041c89c1032
Polina <dfessica@gmail.com>
2025-06-05 11:22:49 +0300

    added new task

---
+++ VoskresenskayaPolina/src/Module3/Hard/task1.java
@@ -0,0 +1,8 @@
+package Module3.Hard;
+
+public class task1 {
+    public static void main(String[] args) {
+     String s = "Hello World!";
+        System.out.println("Выведенная подстрока: " + s.substring(6, 11));
+    }
+}
