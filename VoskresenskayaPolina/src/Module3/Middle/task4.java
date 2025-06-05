2357c880b89b14d3bf9a250a7744883fc629b51d
Polina <dfessica@gmail.com>
2025-06-05 11:00:28 +0300

    added new task

---
+++ VoskresenskayaPolina/src/Module3/Middle/task4.java
@@ -0,0 +1,12 @@
+package Module3.Middle;
+
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любую строку: ");
+        String s = scanner.nextLine();
+        System.out.println("Длинна строки = "+ s.length());
+    }
+}
