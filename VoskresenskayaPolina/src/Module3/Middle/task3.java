087651c280d488574d5be485fe5dd78912752eb6
Polina <dfessica@gmail.com>
2025-06-05 10:56:01 +0300

    added new task

---
+++ VoskresenskayaPolina/src/Module3/Middle/task3.java
@@ -0,0 +1,9 @@
+package Module3.Middle;
+
+public class task3 {
+    public static void main(String[] args) {
+        String s = "Hello Java!";
+        System.out.println("Поиск символа J - " + s.indexOf('J'));
+        System.out.println("Строка в верхнем регистре: "+ s.toUpperCase());
+    }
+}
