d21aff9906d9c4cdb69eb1fa94263b58897b0c3f
Polina <dfessica@gmail.com>
2025-06-05 10:48:41 +0300

    added new task

---
+++ VoskresenskayaPolina/src/Module3/Middle/task2.java
@@ -0,0 +1,17 @@
+package Module3.Middle;
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение для переменной а: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите значение для переменной b: ");
+        int b = scanner.nextInt();
+        System.out.println("Введите значение для переменной c: ");
+        int c = scanner.nextInt();
+        int D = b*b-4*a*c;
+        System.out.println("Дискриминант равен: " +D);
+    }
+}
